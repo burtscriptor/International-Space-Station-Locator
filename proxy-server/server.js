25b2e5b46a2bfd6965b1ae15e1c96b4a6af05d2d
David Burt <157177682+burtscriptor@users.noreply.github.com>
2024-08-05 19:10:48 +1000

    Update server.js

--- proxy-server/server.js
+++ proxy-server/server.js
@@ -5,29 +5,29 @@ const app = express()
 app.get("/api", async (request, response) =>  {
   
     try {
-        const NASA = await axios.get(('http://api.open-notify.org/iss-now.json'))
-        console.log(NASA.data.iss_position)
-        response.json( { 'NASA' : NASA.data.iss_position})
+        const NASA = await axios.get(('http://api.open-notify.org/iss-now.json'));
+        console.log(NASA.data.iss_position);
+        response.json( { 'NASA' : NASA.data.iss_position});
     } catch (error) {
-        console.log(error)
+        console.log(error);
         response.status(500).json({ error: 'Error fetching ISS position' });
-    }
+    };
    
     
 });
 
 app.get("/api/astros", async (request, response) => {
     try {
-        const ASTROs = await axios.get(('http://api.open-notify.org/astros.json'))
-        console.log("this is ASTROs", ASTROs)
-        response.json( { "astros" : ASTROs.data})
+        const ASTROs = await axios.get(('http://api.open-notify.org/astros.json'));
+        console.log("this is ASTROs", ASTROs);
+        response.json( { "astros" : ASTROs.data});
     } catch (error) {
         console.log('astros error', error)
         response.status(500).json({ error: 'Error fetching astros' });
 
-    }
-})
+    };
+});
 
 
-app.listen(5000, () => { console.log("server started on port 5000") })
+app.listen(5000, () => { console.log("server started on port 5000") });
 
